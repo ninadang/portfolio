2b983a9498564fb38fc5a307eba7f39ce02377ea
Nina Dang <ninadangt@gmail.com>
2014-08-01 23:26:23 -0700

    hero done

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$('.black').hide();
-	$('#hero-img').on('mouseenter', function(){
-		$('.black').fadeIn(400);
-	}).on('mouseleave', function(){
-  		$(".black").fadeOut(400);
-	});
-});
